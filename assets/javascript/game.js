f54396123c6f491e8d7b74f3ec1dfe74d5adccfb
Kevin Shipley <35049981+kevin8pizza@users.noreply.github.com>
2018-05-07 12:29:40 -0700

    some css and js additions and pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,17 @@ var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
 
 var letterGuess = '';
 
+var currentWord = '';
+
 //Letters Guessed
-$('#body').keyUp(function() {
-    letterGuess = this; // letterGuess should now equal a string of the key pressed
+$('#body').keyUp(function ( {
+    letterGuess = this; // letterGuess should now equal a string of the key pressed?
     lowerCase(this);// make the letter pressed lower case
     // if -- check 'this' is a letter of the word
-    // else -- otherwise send 'this' to guessed bank
-})
+    if (letterGuess === ) { // ( w || o || r || d )
+
+    } else {
+        // sends letter to letters guessed
+    })
 
 // for loop to add blanks that are then replaced with correctly guessed by player
