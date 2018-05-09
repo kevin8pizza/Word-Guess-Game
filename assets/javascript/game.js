db182b01b96b50fb3abbcdda405cf220bddbbdc1
Kevin Shipley <35049981+kevin8pizza@users.noreply.github.com>
2018-05-08 20:58:49 -0700

    add list and randomization

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,41 @@
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+
+var letters = ["_", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var wordBank = [ 'deathstar', 'skywalker', 'wookie', 'speeder', 'lightsaber', 'ewok', 'hoth', 'droid', 'jedi', 'kenobi', 'coruscant', 'alderaan', 'trooper', 'dewback', 'bantha', 'sandcrawler', 'cloudcity', 'lightspeed', 'kessel', 'yavin', 'tatooine', 'moseisley', 'jawa', 'wompa', 'blaster', 'bothan', 'empire', 'rebel'];
+
+
+var currentWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+
+console.log(currentWord)
+
+var currentSpelling = currentWord.split('');
+
+console.log(currentSpelling);
 
 var letterGuess = '';
+// .toLowerCase()
 
-var currentWord = '';
+// test to generate letters
+document.keyUp = function() {
+    
+    var test = this;
+    console.log(test);
+} 
 
-//Letters Guessed
-$('#body').keyUp(function ( {
-    letterGuess = this; // letterGuess should now equal a string of the key pressed?
-    lowerCase(this);// make the letter pressed lower case
-    // if -- check 'this' is a letter of the word
-    if (letterGuess === ) { // ( w || o || r || d )
+// letter guess function so far
+//document.onkeyUp = function(guess) {
+    //letterGuess.toLowerCase() = this;
+    //if (letterGuess === currentSpelling.indexOf(letterGuess)) {
+        //return true;
+        //console.log('Correct!');
+    //}
+    //else {
+        //return false;
+        //console.log('Incorrect');
+    //}
+//};
 
-    } else {
-        // sends letter to letters guessed
-    })
 
 // for loop to add blanks that are then replaced with correctly guessed by player
+
+// use .split() and .join() to make and break arrays//
